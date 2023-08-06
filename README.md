3a3f03e218abbde0424543098c06aaf7c6312d39
xbkaishui <xxkaishui@gmail.com>
2023-08-06 21:43:30 +0800

    add hello

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cuda-learning
 learn c cuda and pycuda
+
+# code refer 
+https://github.com/PacktPublishing/Hands-On-GPU-Accelerated-Computer-Vision-with-OpenCV-and-CUDA
---
+++ hello/hello.cu
@@ -0,0 +1,10 @@
+#include <iostream>
+__global__ void myfirstkernel(void)
+{
+}
+int main(void)
+{
+    myfirstkernel<<<1, 1>>>();
+    printf("Hello, CUDA!\n");
+    return 0;
+}
